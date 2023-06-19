cfa93bd80a10ae29fcd6f084c8976c7fd95a03f8
Junior Ngangeli <juniorngangeli3@gmail.com>
2023-06-19 12:49:48 +0100

    feat(task): implements task creation controller action

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -33,7 +34,8 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
     }
 
     /**
