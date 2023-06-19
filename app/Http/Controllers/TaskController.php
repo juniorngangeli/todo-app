544ca7b13ce987a745a8c3ad301d652a37e9e39c
Junior Ngangeli <juniorngangeli3@gmail.com>
2023-06-19 13:01:19 +0100

    feat(task): implements task list feature

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,7 +17,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::all();
+        return response()->json($tasks);
     }
 
     /**
